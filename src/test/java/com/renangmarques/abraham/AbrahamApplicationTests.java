3e48e03c1dd1bd2854a48ce6d11c31ef31868f00
Renan Gabriel Marques <renan@debian.renangmarques.com.br>
2020-09-28 17:09:04 -0300

    Abraham project setup

---
+++ src/test/java/com/renangmarques/abraham/AbrahamApplicationTests.java
@@ -0,0 +1,13 @@
+package com.renangmarques.abraham;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AbrahamApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
