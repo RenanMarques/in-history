a7c1592373c0a46260f93902fcedbb3e01dd6a21
Renan Gabriel Marques <renan@debian.renangmarques.com.br>
2020-09-29 13:45:39 -0300

    Replace loggers declaration for lombok annotation

--- src/main/java/com/renangmarques/inhistory/InHistoryApplication.java
+++ src/main/java/com/renangmarques/inhistory/InHistoryApplication.java
@@ -2,8 +2,7 @@ package com.renangmarques.inhistory;
 
 import com.renangmarques.inhistory.model.Movie;
 import com.renangmarques.inhistory.repository.MovieRepository;
-import org.slf4j.Logger;
-import org.slf4j.LoggerFactory;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
@@ -15,10 +14,8 @@ import java.util.List;
 
 @SpringBootApplication
 @EnableNeo4jRepositories
-
+@Slf4j
 public class InHistoryApplication {
-
-	private static Logger logger = LoggerFactory.getLogger(InHistoryApplication.class);
 
 	public static void main(String[] args) {
 		SpringApplication.run(InHistoryApplication.class, args);
@@ -27,20 +24,20 @@ public class InHistoryApplication {
 	@Bean
 	CommandLineRunner initDatabase(MovieRepository movieRepository) {
 		return args -> {
-			logger.debug("Initializing data for application");
+			log.debug("Initializing data for application");
 
-			logger.debug("Removing all data");
+			log.debug("Removing all data");
 			movieRepository.deleteAll();
+			log.debug("All data removed");
 
-			logger.debug("Inserting new data");
+			log.debug("Inserting new data");
 
 			Movie braveheart = new Movie().withTitle("Braveheart");
 			Movie outlawKing = new Movie().withTitle("Outlaw King");
 
-			List<Movie> movies = List.of(
-					new Movie().withTitle("Saving Private Ryan"),
-					new Movie().withTitle("Braveheart"));
+			List<Movie> movies = List.of(braveheart, outlawKing);
 			movieRepository.saveAll(movies);
+			movies.forEach(movie -> {log.debug("Inserting... " + movie);});
 		};
 	}
 
--- src/main/java/com/renangmarques/inhistory/controller/MovieController.java
+++ src/main/java/com/renangmarques/inhistory/controller/MovieController.java
@@ -3,12 +3,14 @@ package com.renangmarques.inhistory.controller;
 import com.renangmarques.inhistory.model.Movie;
 import com.renangmarques.inhistory.service.MovieService;
 import lombok.AllArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import java.util.Collection;
 
+@Slf4j
 @AllArgsConstructor
 @RestController
 @RequestMapping(path = "/movies")
